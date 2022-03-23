55c4af7246f328baf2ef0a8b4eae78957227afa2
Steve-Morales <sam008@ucsd.edu>
2022-03-22 23:38:17 -0700

    Removed Chromium From Project - Updated API

--- API.py
+++ API.py
@@ -1,17 +1,25 @@
+# install: selenium, webdriver_manager
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import os
-
-## path to chrome browser relative to file project ##
-CHROMIUM_PATH = "Win_Chrome\chrome-win\chrome-win\chrome.exe"
 
 ## options for driver/browser ##
 opts = Options()
 # opts.add_argument("--headless")               # uncomment to run as headless
-opts.add_argument("--window-size=1920x1080")    # size of browser
+opts.add_argument("start-maximized")          # starts browser maximized
 
 ## webdriver/browser with defined options/settings ##
-driver = webdriver.Chrome(chrome_options=opts, executable_path=CHROMIUM_PATH)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=opts)
+driver.implicitly_wait(15) # will wait up to 15 seconds for things to load when grabbing elements
+
+DEFAULT_URL = "https://twitter.com/"    # default url **do NOT change!**
+page = "mojang"                         # twitter user's profile page
+
+## go to webpage ##
+driver.get(DEFAULT_URL+page)
+
+driver.close()
 
 
-driver.get("https://google.com")
